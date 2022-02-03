bfb70dfb6433e14d8c536457c88734fdabdd9fce
jnicholCU <joshua.nicholson@colorado.edu>
2022-02-03 09:56:46 -0700

    Update ucb-equalize-height.js
    Reformatted for lint

--- js/ucb-equalize-height.js
+++ js/ucb-equalize-height.js
@@ -4,7 +4,8 @@ function setHeight(el, val) {
   else el.style.height = val + "px";
 }
 
-var equalheight = function (container) { // eslint-disable-line
+var equalheight = function (container) {
+  // eslint-disable-line
   var currentTallest = 0,
     currentRowStart = 0,
     rowDivs = new Array(),
@@ -12,7 +13,8 @@ var equalheight = function (container) { // eslint-disable-line
     topPosition = 0,
     currentDiv = 0;
 
-  Array.from(document.querySelectorAll(container)).forEach((el, i) => { // eslint-disable-line
+  Array.from(document.querySelectorAll(container)).forEach((el, i) => {
+    // eslint-disable-line
     el.style.height = "auto";
     topPosition = el.offsetTop;
     if (currentRowStart != topPosition) {
@@ -42,56 +44,56 @@ var equalheight = function (container) { // eslint-disable-line
 /* 
   Classes to equalize Column Layout images using ucb-equalize-height.js
 */
-	window.addEventListener("load", function () {
-equalheight('.grid-column .grid-image-container article')
-})
+window.addEventListener("load", function () {
+  equalheight(".grid-column .grid-image-container article");
+});
 window.addEventListener("resize", function () {
-setTimeout(function () {
-equalheight('.grid-column .grid-image-container article')
-})
-})
+  setTimeout(function () {
+    equalheight(".grid-column .grid-image-container article");
+  });
+});
 
 /* 
   Classes to equalize Card Layout fields using ucb-equalize-height.js
 */
-	window.addEventListener("load", function () {
-equalheight('.grid-image-container article')
-})
+window.addEventListener("load", function () {
+  equalheight(".grid-image-container article");
+});
 window.addEventListener("resize", function () {
-setTimeout(function () {
-equalheight('.grid-image-container article')
-})
-})
+  setTimeout(function () {
+    equalheight(".grid-image-container article");
+  });
+});
 
 window.addEventListener("load", function () {
-equalheight('.grid-card')
-})
+  equalheight(".grid-card");
+});
 window.addEventListener("resize", function () {
-setTimeout(function () {
-equalheight('.grid-card')
-})
-})
+  setTimeout(function () {
+    equalheight(".grid-card");
+  });
+});
 
 /* 
   Classes to equalize Overlay Layout images using ucb-equalize-height.js
 */
-	window.addEventListener("load", function () {
-equalheight('.overlay-grid-image-container article')
-})
+window.addEventListener("load", function () {
+  equalheight(".overlay-grid-image-container article");
+});
 window.addEventListener("resize", function () {
-setTimeout(function () {
-equalheight('.overlay-grid-image-container article')
-})
-})
+  setTimeout(function () {
+    equalheight(".overlay-grid-image-container article");
+  });
+});
 
 /* 
   Classes to equalize using ucb-equalize-height.js
 */
-	window.addEventListener("load", function () {
-equalheight('.grid-image-container-small .grid-image-container article')
-})
+window.addEventListener("load", function () {
+  equalheight(".grid-image-container-small .grid-image-container article");
+});
 window.addEventListener("resize", function () {
-setTimeout(function () {
-equalheight('.grid-image-container-small .grid-image-container article')
-})
-})
+  setTimeout(function () {
+    equalheight(".grid-image-container-small .grid-image-container article");
+  });
+});
