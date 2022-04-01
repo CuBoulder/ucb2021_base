ad51d87c5f853963d03fac43b0405f025cd04c16
Jacob Korf <korfj921921@gmail.com>
2022-04-01 08:58:40 -0600

    glightbox all easy js lint

--- js/glightbox.js
+++ js/glightbox.js
@@ -2,8 +2,8 @@
   typeof exports === "object" && typeof module !== "undefined"
     ? (module.exports = factory())
     : typeof define === "function" && define.amd
-    ? define(factory)
-    : ((global = global || self), (global.GLightbox = factory()));
+      ? define(factory)
+      : ((global = global || self), (global.GLightbox = factory()));
 })(this, function () {
   "use strict";
 
@@ -441,7 +441,7 @@
     var found;
 
     if (url.indexOf(".css") !== -1) {
-      found = document.querySelectorAll('link[href="' + url + '"]');
+      found = document.querySelectorAll("link[href='" + url + "']");
 
       if (found && found.length > 0) {
         if (isFunction(callback)) {
@@ -452,7 +452,7 @@
       }
 
       var head = document.getElementsByTagName("head")[0];
-      var headStyles = head.querySelectorAll('link[rel="stylesheet"]');
+      var headStyles = head.querySelectorAll("link[rel='stylesheet']");
       var link = document.createElement("link");
       link.rel = "stylesheet";
       link.type = "text/css";
@@ -472,7 +472,7 @@
       return;
     }
 
-    found = document.querySelectorAll('script[src="' + url + '"]');
+    found = document.querySelectorAll("script[src='" + url + "']");
 
     if (found && found.length > 0) {
       if (isFunction(callback)) {
@@ -622,7 +622,7 @@
     });
     var nextFocus = nextOrders.sort()[0];
     return document.querySelector(
-      '.gbtn[data-taborder="'.concat(nextFocus, '"]')
+      ".gbtn[data-taborder='".concat(nextFocus, "']")
     );
   }
 
@@ -1088,8 +1088,8 @@
               ? "Left"
               : "Right"
             : y1 - y2 > 0
-            ? "Up"
-            : "Down";
+              ? "Up"
+              : "Down";
         },
       },
       {
@@ -2091,11 +2091,11 @@
       var placeholder = customPlaceholder
         ? customPlaceholder
         : createHTML(
-          '<div id="'
-            .concat(videoID, '" data-plyr-provider="')
-            .concat(provider, '" data-plyr-embed-id="')
-            .concat(url, '"></div>')
-          );
+          "<div id='"
+            .concat(videoID, "' data-plyr-provider='")
+            .concat(provider, "' data-plyr-embed-id='")
+            .concat(url, "'></div>")
+        );
       addClass(videoWrapper, "".concat(provider, "-video gvideo"));
       videoWrapper.appendChild(placeholder);
       videoWrapper.setAttribute("data-id", videoID);
@@ -2153,7 +2153,7 @@
     if (content) {
       if (isString(content)) {
         innerContent = createHTML(
-          '<div class="ginlined-content">'.concat(content, "</div>")
+          "<div class='ginlined-content'>".concat(content, "</div>")
         );
       }
 
@@ -2683,7 +2683,7 @@
           div = null;
           return (
             subString +
-            '... <a href="#" class="desc-more">' +
+            "... <a href='#' class='desc-more'>" +
             wordBoundary +
             "</a>"
           );
@@ -2859,15 +2859,15 @@
     },
     svg: {
       close:
-        '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" xml:space="preserve"><g><g><path d="M505.943,6.058c-8.077-8.077-21.172-8.077-29.249,0L6.058,476.693c-8.077,8.077-8.077,21.172,0,29.249C10.096,509.982,15.39,512,20.683,512c5.293,0,10.586-2.019,14.625-6.059L505.943,35.306C514.019,27.23,514.019,14.135,505.943,6.058z"/></g></g><g><g><path d="M505.942,476.694L35.306,6.059c-8.076-8.077-21.172-8.077-29.248,0c-8.077,8.076-8.077,21.171,0,29.248l470.636,470.636c4.038,4.039,9.332,6.058,14.625,6.058c5.293,0,10.587-2.019,14.624-6.057C514.018,497.866,514.018,484.771,505.942,476.694z"/></g></g></svg>',
-      next: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 477.175 477.175" xml:space="preserve"> <g><path d="M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z"/></g></svg>',
-      prev: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 477.175 477.175" xml:space="preserve"><g><path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"/></g></svg>',
+        "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 512 512' xml:space='preserve'><g><g><path d='M505.943,6.058c-8.077-8.077-21.172-8.077-29.249,0L6.058,476.693c-8.077,8.077-8.077,21.172,0,29.249C10.096,509.982,15.39,512,20.683,512c5.293,0,10.586-2.019,14.625-6.059L505.943,35.306C514.019,27.23,514.019,14.135,505.943,6.058z'/></g></g><g><g><path d='M505.942,476.694L35.306,6.059c-8.076-8.077-21.172-8.077-29.248,0c-8.077,8.076-8.077,21.171,0,29.248l470.636,470.636c4.038,4.039,9.332,6.058,14.625,6.058c5.293,0,10.587-2.019,14.624-6.057C514.018,497.866,514.018,484.771,505.942,476.694z'/></g></g></svg>",
+      next: "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 477.175 477.175' xml:space='preserve'> <g><path d='M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z'/></g></svg>",
+      prev: "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 477.175 477.175' xml:space='preserve'><g><path d='M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z'/></g></svg>",
     },
   };
   defaults.slideHTML =
-    '<div class="gslide">\n    <div class="gslide-inner-content">\n        <div class="ginner-container">\n            <div class="gslide-media">\n            </div>\n            <div class="gslide-description">\n                <div class="gdesc-inner">\n                    <h4 class="gslide-title"></h4>\n                    <div class="gslide-desc"></div>\n                </div>\n            </div>\n        </div>\n    </div>\n</div>';
+    "<div class='gslide'>\n    <div class='gslide-inner-content'>\n        <div class='ginner-container'>\n            <div class='gslide-media'>\n            </div>\n            <div class='gslide-description'>\n                <div class='gdesc-inner'>\n                    <h4 class='gslide-title'></h4>\n                    <div class='gslide-desc'></div>\n                </div>\n            </div>\n        </div>\n    </div>\n</div>";
   defaults.lightboxHTML =
-    '<div id="glightbox-body" class="glightbox-container" tabindex="-1" role="dialog" aria-hidden="false">\n    <div class="gloader visible"></div>\n    <div class="goverlay"></div>\n    <div class="gcontainer">\n    <div id="glightbox-slider" class="gslider"></div>\n    <button class="gclose gbtn" aria-label="Close" data-taborder="3">{closeSVG}</button>\n    <button class="gprev gbtn" aria-label="Previous" data-taborder="2">{prevSVG}</button>\n    <button class="gnext gbtn" aria-label="Next" data-taborder="1">{nextSVG}</button>\n</div>\n</div>';
+    "<div id='glightbox-body' class='glightbox-container' tabindex='-1' role='dialog' aria-hidden='false'>\n    <div class='gloader visible'></div>\n    <div class='goverlay'></div>\n    <div class='gcontainer'>\n    <div id='glightbox-slider' class='gslider'></div>\n    <button class='gclose gbtn' aria-label='Close' data-taborder='3'>{closeSVG}</button>\n    <button class='gprev gbtn' aria-label='Previous' data-taborder='2'>{prevSVG}</button>\n    <button class='gnext gbtn' aria-label='Next' data-taborder='1'>{nextSVG}</button>\n</div>\n</div>";
 
   var GlightboxInit = (function () {
     function GlightboxInit() {
