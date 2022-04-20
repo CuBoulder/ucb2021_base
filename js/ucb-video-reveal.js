357c028c84411429d051802c7d77cad075e94a0c
Jacob Korf <korfj921921@gmail.com>
2022-04-20 13:48:35 -0500

    working slightly, bad implementation

---
+++ js/ucb-video-reveal.js
@@ -0,0 +1,7 @@
+var videoImage = document.getElementById("video-image-div");
+var videoVideo = document.getElementById("video_reveal");
+
+function videoRevealReplacement() {
+videoImage.style.display = "none";
+videoVideo.style.display = "block";
+}
