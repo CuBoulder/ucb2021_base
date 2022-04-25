ddf108a8223c39aa6c7539e6502a1af8a42f4698
jnicholCU <joshua.nicholson@colorado.edu>
2022-04-25 14:04:02 -0600

    Update ucb-equalize-height.js
    Update for lint fix

--- js/ucb-equalize-height.js
+++ js/ucb-equalize-height.js
@@ -1,4 +1,4 @@
-function setHeight(el, val, container) {
+function setHeight(el, val) {
   if (typeof val === "function") val = val();
   if (typeof val === "string") el.style.height = val;
   else el.style.height = val + "px";
