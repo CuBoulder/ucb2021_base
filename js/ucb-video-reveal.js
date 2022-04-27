6b948b4f91e711b64e8b45022f321a5aac2be4d6
Jeremy C. Sparks <jesp3304@colorado.edu>
2022-04-27 16:42:27 -0600

    chg: stupid linting errors

--- js/ucb-video-reveal.js
+++ js/ucb-video-reveal.js
@@ -1,16 +1,16 @@
 /*
  *  ucb-video-reveal.js - This should allow the dynamic functionality necessary
- *      for the video reveal paragraph.   
- * 
- *  FUTURE  : add in auto-play of vidoe on reveal 
- *          : have image fade-out and video fade-in?  
+ *      for the video reveal paragraph.
+ *
+ *  FUTURE  : add in auto-play of vidoe on reveal
+ *          : have image fade-out and video fade-in?
 */
 var els = document.getElementsByClassName("ucb-video-reveal-controls");
 
-// Adding a handler for each instance of the video reveal ... 
+// Adding a handler for each instance of the video reveal ...
 // this should allow multiple instances of the video reveal paragraph
-// to all work independantly on the page.  
-for(var i = 0, x = els.length; i < x; i++) {
+// to all work independantly on the page.
+for (let i = 0, x = els.length; i < x; i++) {
   els[i].onclick = function() {
     // sanity checker
     // alert("Coming Soon!");
@@ -20,28 +20,27 @@ for(var i = 0, x = els.length; i < x; i++) {
     // we then have a sibling elements of the image (hide)
     // and the video (show)
 
-    // get the parent element 
+    // get the parent element
     let myParentEl = this.parentElement;
 
     if(myParentEl) {
-      // find the text controls and hide them 
+      // find the text controls and hide them
       myParentEl.querySelector(".ucb-video-reveal-controls").style.display = "none";
 
-      // find the image and hide it 
+      // find the image and hide it
       myParentEl.querySelector(".ucb-video-reveal-image").style.display = "none";
 
-      // find the video and show it 
-      let videoEl = myParentEl.querySelector(".ucb-video-reveal-video"); 
+      // find the video and show it
+      let videoEl = myParentEl.querySelector(".ucb-video-reveal-video");
       videoEl.style.display = "block";
 
-      // now we should be able to play the video for the user 
-      // I wonder how that works... 
+      // now we should be able to play the video for the user
+      // I wonder how that works...
       // looks like you can add "&autoplay=1" to the src field to make a YouTube
       // video autoplay.
 
-      // apparently this is difficult -- maybe future polish? 
+      // apparently this is difficult -- maybe future polish?
 
-    ;}
-
-  }
+    }
+  };
 }
