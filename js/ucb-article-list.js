c6a8df4526a6da5cdaa5de62724941ff0c41253c
Pat Brown <breepatbree@gmail.com>
2022-05-20 15:33:35 -0600

    wip: exclude filters

--- js/ucb-article-list.js
+++ js/ucb-article-list.js
@@ -2,12 +2,16 @@ async function getArticleParagraph(id){
     return await fetch(`/jsonapi/paragraph/article_content/${id}?include[paragraph--article_content]=field_article_image,field_article_text&include=field_article_image.field_media_image&fields[file--file]=uri,url`)
 }
 
-function intersect(a, b) {
-    var setB = new Set(b);
-    return [...new Set(a)].filter(x => setB.has(x));
-}
+// function intersect(a, b) {
+//     var setB = new Set(b);
+//     return [...new Set(a)].filter(x => setB.has(x));
+// }
 
 function renderArticleList(JSONURL,id='ucb-article-listing',ExcludeCategories='',ExcludeTags=''){
+
+    let excludeCatArray = ExcludeCategories.split(',').map(Number)
+    let excludeTagArray = ExcludeTags.split(',').map(Number)
+
     if(JSONURL) {
         let el = document.getElementById(id);
 
@@ -51,6 +55,35 @@ function renderArticleList(JSONURL,id='ucb-article-listing',ExcludeCategories=''
 
                 //iterate over each item in the array
                 data.data.map((item)=> {
+                    let thisArticleCats = [];
+                    let thisArticleTags = [];
+                    // // loop through and grab all of the categories
+                    if(item.relationships.field_ucb_article_categories) {
+                        for(var i = 0; i < item.relationships.field_ucb_article_categories.data.length; i++ ) {
+                          thisArticleCats.push(item.relationships.field_ucb_article_categories.data[i].meta.drupal_internal__target_id); 
+                        }
+                    }
+                    // console.log("this article cats",thisArticleCats)
+
+                    // // loop through and grab all of the tags 
+                    if(item.relationships.field_ucb_article_tags) {
+                        for(var i = 0; i < item.relationships.field_ucb_article_tags.data.length; i++ ) {
+                          thisArticleTags.push(item.relationships.field_ucb_article_tags.data[i].meta.drupal_internal__target_id); 
+                        }
+                    }
+                    // console.log('this article tags',thisArticleTags)
+
+                    // checks to see if the current article (item) contains a category or tag scheduled for exclusion
+                    let doesIncludeCat = thisArticleCats.filter(element => excludeCatArray.includes(element));
+                    let doesIncludeTag = thisArticleTags.filter(element => excludeTagArray.includes(element));
+
+                    console.log(excludeCatArray, thisArticleCats,doesIncludeCat)
+                    console.log(excludeTagArray,thisArticleTags,doesIncludeTag)
+
+                    // if the doesInclude check for tags or cats returns any number, don't proceed. Else, we want to build the page
+                    if(!doesIncludeCat.length==0 || !doesIncludeTag.length==0){
+                        console.log("was excluded", item)
+                    } else {
                     //create container for elements
                     let elDiv = document.createElement("div")
                     document.getElementById('block-ucb2021-base-content').appendChild(elDiv)
@@ -66,42 +99,27 @@ function renderArticleList(JSONURL,id='ucb-article-listing',ExcludeCategories=''
                     // **ADD DATA**
                     // this is my id of the article body paragraph type we need only if no thumbnail or summary provided
                     let bodyAndImageId = item.relationships.field_ucb_article_content.data[0].id
-                    let myCategories = [];
-                    let myTags = [];
-                    
-                    // loop through and grab all of the categories
-                    if(item.relationships.field_ucb_article_categories) {
-                        for(var i = 0; i < item.relationships.field_ucb_article_categories.data.length; i++ ) {
-                          myCategories.push(parseInt(item.relationships.field_ucb_article_categories.data[i].meta.drupal_internal__target_id)); 
-                        }
-                    }
 
-                    alert("My Categories are : "  + myCategories);
 
-                    // loop through and grab all of the tags 
-                    if(item.relationships.field_ucb_article_tags) {
-                        for(var i = 0; i < item.relationships.field_ucb_article_tags.data.length; i++ ) {
-                          myTags.push(item.relationships.field_ucb_article_tags.data[i].meta.drupal_internal__target_id); 
-                        }
-                    }
+                
 
-                    // if one of our categories matches one of the excluded categories... bail and continue with the next one
-                    let checkCats = ExcludeCategories.split(',').filter(cat => parseInt(cat));
-                    alert("Checking Categories for exclusion : " + checkCats);
-                    // const CatMatch = myCategories.filter(value => checkCats.includes(value));
-                    // let CatMatch = intersect(myCategories, checkCats);
-                    let CatMatch = false;
-                    console.log("I'm here!!!");
-                    for(var i = 0; i < myCategories.length; i++) {
-                        console.log("My Categories["+i+"] = :"+myCategories[i]+":");
-                        if(checkCats.includes(myCategories[i])) {
-                            console.log("Match found : " + myCategories[i]);
-                            CatMatch = true;
-                        }
-                    }
-                    for(var i = 0; i < checkCats.length; i++) {
-                        console.log("Check Categories["+i+"] = :"+checkCats[i]+":");
-                    }
+                    // // if one of our categories matches one of the excluded categories... bail and continue with the next one
+                    // let checkCats = ExcludeCategories.split(',').filter(cat => parseInt(cat));
+                    // alert("Checking Categories for exclusion : " + checkCats);
+                    // // const CatMatch = myCategories.filter(value => checkCats.includes(value));
+                    // // let CatMatch = intersect(myCategories, checkCats);
+                    // let CatMatch = false;
+                    // console.log("I'm here!!!");
+                    // for(var i = 0; i < myCategories.length; i++) {
+                    //     console.log("My Categories["+i+"] = :"+myCategories[i]+":");
+                    //     if(checkCats.includes(myCategories[i])) {
+                    //         console.log("Match found : " + myCategories[i]);
+                    //         CatMatch = true;
+                    //     }
+                    // }
+                    // for(var i = 0; i < checkCats.length; i++) {
+                    //     console.log("Check Categories["+i+"] = :"+checkCats[i]+":");
+                    // }
 
                     // alert("Matching Categories are : " + CatMatch);
 
@@ -176,7 +194,7 @@ function renderArticleList(JSONURL,id='ucb-article-listing',ExcludeCategories=''
                     contentDiv.appendChild(contentDate)
                     contentDiv.appendChild(contentBody)
                     contentDiv.appendChild(contentLink)
-                })
+                }})
             // remove loading text
             el.innerText = "";
             });
@@ -193,8 +211,6 @@ function renderArticleList(JSONURL,id='ucb-article-listing',ExcludeCategories=''
         JSONURL = el.dataset.jsonapi;
         CategoryExclude = el.dataset.excats;
         TagsExclude = el.dataset.extags;
-
-        alert("You want to exclude these tags : " + TagsExclude);
     }
 
    renderArticleList(JSONURL,'ucb-article-listing',CategoryExclude, TagsExclude);
