c9204a29b454fb44d0872c61d19bffbd270dc977
Jeremy C. Sparks <jesp3304@colorado.edu>
2022-05-31 21:39:10 -0600

    wip: Working on Person List node

--- js/ucb-people-list.js
+++ js/ucb-people-list.js
@@ -1,8 +1,9 @@
 /* naughty global variables!!! */
-let Departments = {}
-let JobTypes = {}
-let ourPepole = {}
-let renderedTable = 0; 
+let Departments = {} // translate table for department id => department name
+let JobTypes = {} // translate table for type id => type name
+let ourPepole = {} // all of the pepole that match our filter 
+let renderedTable = 0;  // flag to know if we've rendered the table header or not yet
+let firstPassCount = 0; // count for the first pass per group.  
 
 function toggleMessage(id, display = 'none') {
   if (id) {
@@ -21,17 +22,21 @@ function toggleMessage(id, display = 'none') {
 function getTaxonomy(taxonomyName) {
   return new Promise(function (resolve, reject) {
     if (taxonomyName) {
-      let result = {}
-      let taxonomyURL = `/jsonapi/taxonomy_term/${taxonomyName}`
+      let result = [] 
+      let taxonomyURL = `/jsonapi/taxonomy_term/${taxonomyName}?sort=weight,name`
 
       fetch(taxonomyURL)
         .then((response) => response.json())
         .then((data) => {
+          console.log("Taxonomy Object : ", data)
           data.data.map((attributes) => {
-            let key = attributes.attributes.drupal_internal__tid
-            let value = attributes.attributes.name
+            let id = attributes.attributes.drupal_internal__tid
+            let name = attributes.attributes.name
+            let thisTerm = {}
 
-            result[key] = value
+            thisTerm['id'] = id
+            thisTerm['name'] = name
+            result.push(thisTerm) 
           })
 
           resolve(result)
@@ -43,12 +48,17 @@ function getTaxonomy(taxonomyName) {
   })
 }
 
+function getTaxonomyName(taxonomy, tid) {
+  return taxonomy.find( ({ id }) => id === tid );
+}
+
 function getStaff(JSONAPI) {
   return new Promise(function (resolve, reject) {
     if (JSONAPI) {
       fetch(JSONAPI)
         .then((response) => response.json())
         .then((data) => {
+          console.log("Person Data is : ", data)
           resolve(data)
         })
     } else {
@@ -149,7 +159,12 @@ function displayPersonCard(Format, Person) {
   // note: there may be a race condidtion here as we're also querying
   //  to get those friendly names from the API endpoint.
   // console.log('Departments :', Departments)
-  let myDept = Person.Dept ? Departments[Person.Dept] : ''
+  let myDept = ""
+  for(let i = 0; i < Person.Dept.length; i++) {
+    let thisDeptID = Person.Dept[i]
+    let thisDeptName = getTaxonomyName(Departments, thisDeptID).name;
+    myDept += `<li>${thisDeptName}</li>`
+  }
   let myPhoto = ''
   if (Person.PhotoURL) {
     myPhoto = `<img src="${Person.PhotoURL}"  />`
@@ -172,7 +187,9 @@ function displayPersonCard(Format, Person) {
                             ${Person.Title ? Person.Title : ''}
                         </span>
                     <span class="ucb-person-card-dept">
+                      <ul class="ucb-person-card-dept-ul">
                         ${myDept} 
+                      </ul>
                     </span>
                     <span class="ucb-person-card-body">
                         ${Person.Body ? Person.Body : ''}
@@ -218,7 +235,9 @@ function displayPersonCard(Format, Person) {
                   ${Person.Title ? Person.Title : ''}
                 </span>
                 <span class="ucb-person-card-dept departments-grid">
+                  <ul class="ucb-person-card-dept-ul">
                    ${myDept}
+                  </ul>
                  </span>
                 </div>
                 </div>
@@ -240,7 +259,9 @@ function displayPersonCard(Format, Person) {
                   ${Person.Title ? Person.Title : ''}
                 </span>
                 <span class="ucb-person-card-dept departments-grid">
+                  <ul class="ucb-person-card-dept-ul">
                    ${myDept}
+                  </ul>
                  </span>
                   </td>
                   <td>
@@ -272,11 +293,16 @@ function displayPersonCard(Format, Person) {
   return cardHTML
 }
 
-function displayPeople(DISPLAYFORMAT, GROUPBY, groupID) {
-  console.log('Group by : ' + GROUPBY)
-  console.log('Group ID is : ' + groupID)
+function displayPeople(DISPLAYFORMAT, GROUPBY, groupID, ORDERBY) {
   let renderThisGroup = 0; 
   let el = document.getElementById('ucb-people-list-page')
+  let thisDeptName = ""
+  let thisTypeName = ""
+  if(GROUPBY === "department") {
+    thisDeptName = getTaxonomyName(Departments, groupID).name
+  } else if(GROUPBY === "type") {
+    thisTypeName = getTaxonomyName(JobTypes, groupID).name
+  }
   el.classList = 'container'
   // let headerHTML = layoutHeader(DISPLAYFORMAT)
   // let footerHTML = layoutFooter(DISPLAYFORMAT)
@@ -285,6 +311,12 @@ function displayPeople(DISPLAYFORMAT, GROUPBY, groupID) {
   // TO DO -- issue here with header adding correctly to grid
   if (DISPLAYFORMAT === 'grid' || DISPLAYFORMAT === 'table') {
     el.appendChild(parentContainer)
+  }
+
+  // if this is our second pass on rendering content we don't need a group-by title 
+  // we also don't some of the opening DOM elements before the card info.  
+  if(ORDERBY === "secondpass" && firstPassCount) {
+    renderThisGroup++;
   }
 
   // fetch(JSONURL)
@@ -318,14 +350,23 @@ function displayPeople(DISPLAYFORMAT, GROUPBY, groupID) {
     // maps over data
     ourPepole.data.map((person) => {
       // get the person data we need
+      let renderMe = true;
       let thisPerson = {}
       let thisPersonCard = '' // placeholder for the HTML to render this card in the required format
       thisPerson['Name'] = person.attributes.title
       thisPerson['Title'] = person.attributes.field_ucb_person_title[0]
-      thisPerson['Dept'] =
-        person.relationships.field_ucb_person_department.data[0].meta.drupal_internal__target_id
-      thisPerson['Jobtype'] =
-        person.relationships.field_ucb_person_job_type.data[0].meta.drupal_internal__target_id
+      thisPerson['Dept'] = []
+      for(let i = 0; i < person.relationships.field_ucb_person_department.data.length; i++) {
+        thisPerson['Dept'].push(
+          person.relationships.field_ucb_person_department.data[i].meta.drupal_internal__target_id
+        );
+      } 
+      thisPerson['Jobtype'] = []
+      for(let i = 0; i < person.relationships.field_ucb_person_job_type.data.length; i++) {
+        thisPerson['Jobtype'].push(
+          person.relationships.field_ucb_person_job_type.data[i].meta.drupal_internal__target_id
+        );
+      }
       thisPerson['PhotoID'] =
         person.relationships.field_ucb_person_photo.data.id
       thisPerson['PhotoURL'] = ''
@@ -347,11 +388,28 @@ function displayPeople(DISPLAYFORMAT, GROUPBY, groupID) {
         // console.log('Am I an image URL? : ' + thisPerson.PhotoURL)
       }
 
+      // if first pass (sort by type first) then only render the people with a Job Type 
+      // else if second pass (sort by type first) then only render the people without a Job Type
+      if(ORDERBY === "firstpass" && !thisPerson['Jobtype'].length) {
+        renderMe = false;
+      } else if(ORDERBY === "secondpass" && thisPerson['Jobtype'].length) {
+        renderMe = false;
+      }
+
+      // check to see if this person needs to be rendered 
+      if(renderMe) {
+
+
       // check to see if we need to filter based on a group by seeting
       // and if so that this person matches our groupID
-      if ((!GROUPBY || !groupID) || (groupID == thisPerson['Dept'] || groupID == thisPerson['Jobtype'])) {
-        console.log( "I'm a match! " + groupID + ' = ' + thisPerson['Dept'] + ' or ' + thisPerson['Jobtype'],)
+      if ((!GROUPBY || !groupID) || (thisPerson['Dept'].find(deptid => deptid == groupID) || thisPerson['Jobtype'].find(typeid => typeid == groupID))) {
+        //console.log( "I'm a match! " + groupID + ' = ' + thisPerson['Dept'] + ' or ' + thisPerson['Jobtype'],)
+
+        // increment flags for rendering in this group as necessary
         renderThisGroup++; 
+        if(ORDERBY === "firstpass") {
+          firstPassCount++;
+        }
 
         thisPersonCard = displayPersonCard(DISPLAYFORMAT, thisPerson)
 
@@ -370,10 +428,16 @@ function displayPeople(DISPLAYFORMAT, GROUPBY, groupID) {
           // check to see if this is the first time we're adding in a member of this group
           // if so, add the name of the group first 
           if(renderThisGroup === 1 && groupID) {
-            let GroupTitle = document.createElement('div');
-            GroupTitle.innerHTML = ` 
-              <h2>${GROUPBY == 'type' ? JobTypes[groupID] : Departments[groupID]}</h2>`
-            el.appendChild(GroupTitle);
+            // we may be on a second pass and have already rendered the title in the first pass
+            // if so, skip the title so we don't end up with two 
+            console.log("First Pass Count is : " + firstPassCount)
+            console.log("Render pass is : " + ORDERBY)
+            if((ORDERBY === "secondpass" && !firstPassCount) || ORDERBY != "secondpass") {
+              let GroupTitle = document.createElement('div');
+              GroupTitle.innerHTML = ` 
+                <h2>${GROUPBY == 'type' ? thisTypeName : thisDeptName}</h2>`
+              el.appendChild(GroupTitle);
+            }
           }
 
           el.appendChild(thisCard)
@@ -382,13 +446,17 @@ function displayPeople(DISPLAYFORMAT, GROUPBY, groupID) {
           // check to see if this is the first time we're adding in a member of this group
           // if so, add the name of the group first 
           if(renderThisGroup === 1 && groupID) {
-            let GroupTitle = document.createElement('div');
-            GroupTitle.classList = "col-12";
-            GroupTitle.innerHTML = `<h2>${GROUPBY == 'type' ? JobTypes[groupID] : Departments[groupID]}</h2>`
-            parentContainer.appendChild(GroupTitle)
+            // we may be on a second pass and have already rendered the title in the first pass
+            // if so, skip the title so we don't end up with two 
+            if((ORDERBY === "secondpass" && !firstPassCount) || ORDERBY != "secondpass") {
+              let GroupTitle = document.createElement('div');
+              GroupTitle.classList = "col-12";
+              GroupTitle.innerHTML = `<h2>${GROUPBY == 'type' ? thisTypeName : thisDeptName}</h2>`
+              parentContainer.appendChild(GroupTitle)
+            }
           }
 
-          thisCard.classList = 'col-sm-4'
+          thisCard.classList = 'col-sm-12 col-md-4'
           thisCard.innerHTML = thisPersonCard
 
           parentContainer.appendChild(thisCard)
@@ -401,21 +469,26 @@ function displayPeople(DISPLAYFORMAT, GROUPBY, groupID) {
           // check to see if this is the first time we're adding in a member of this group
           // if so, add the name of the group first 
           if(renderThisGroup === 1 && groupID) { 
-            let GroupTitle = document.createElement('tr');
-            let GroupTitleHTML = `
-              <th colspan="3" class="ucb-people-list-group-title-th">
-              ${GROUPBY == 'type' ? JobTypes[groupID] : Departments[groupID]}
-              </th>
-              `
-            GroupTitle.innerHTML = GroupTitleHTML;
-            tablebody.appendChild(GroupTitle);
+            // we may be on a second pass and have already rendered the title in the first pass
+            // if so, skip the title so we don't end up with two 
+            if((ORDERBY === "secondpass" && !firstPassCount) || ORDERBY != "secondpass") {
+              let GroupTitle = document.createElement('tr');
+              let GroupTitleHTML = `
+                <th colspan="3" class="ucb-people-list-group-title-th">
+                ${GROUPBY == 'type' ? thisTypeName : thisDeptName}
+                </th>
+                `
+              GroupTitle.innerHTML = GroupTitleHTML;
+              tablebody.appendChild(GroupTitle);
+            }
           }
           
           tablebody.appendChild(thisCard)
         }
       } else {
-        console.log( 'Not a match! ' + groupID + ' != ' + thisPerson['Dept'] + ' or ' + thisPerson['Jobtype'],);
+        // console.log( 'Not a match! ' + groupID + ' != ' + thisPerson['Dept'] + ' or ' + thisPerson['Jobtype'],);
       }
+    }
     })
   } else {
     console.log('empty staff object, no people to render ', ourPepole)
@@ -427,21 +500,23 @@ function displayPeople(DISPLAYFORMAT, GROUPBY, groupID) {
 }
 
 // INIT
-;(function () {
+(function () {
   let el = document.getElementById('ucb-people-list-page')
   let JSONAPI = el.dataset.json ? el.dataset.json : ''
   let FORMAT = el.dataset.format ? el.dataset.format : ''
   let GROUPBY = el.dataset.groupby ? el.dataset.groupby : ''
+  let ORDERBY = el.dataset.orderby ? el.dataset.orderby : ''
   console.log('Init')
+  console.log("Order by is : " + ORDERBY)
 
   getTaxonomy('department').then((response) => {
     Departments = response
-    //console.log('Our Departments are : ', Departments);
+    // console.log('Our Departments are : ', Departments);
   })
 
   getTaxonomy('ucb_person_job_type').then((response) => {
     JobTypes = response
-    //console.log('Our Job types are : ', JobTypes);
+    console.log('Our Job types are : ', JobTypes);
   })
 
   toggleMessage('ucb-al-loading', 'block')
@@ -454,14 +529,41 @@ function displayPeople(DISPLAYFORMAT, GROUPBY, groupID) {
       toggleMessage('ucb-al-loading', 'none')
       if (GROUPBY == 'department') {
         for (const [key, value] of Object.entries(Departments)) {
-          displayPeople(FORMAT, GROUPBY, key)
+          // let thisDeptID = Departments[key].id
+          // let thisDeptName = getTaxonomyName(Departments, thisDeptID);
+          // console.log("Group by Dept : " + thisDeptID)
+          // console.log("Group by Dept : " + thisDeptName.name)
+
+          if(ORDERBY === "type") {
+            firstPassCount = 0; 
+            displayPeople(FORMAT, GROUPBY, Departments[key].id, "firstpass")
+            displayPeople(FORMAT, GROUPBY, Departments[key].id, "secondpass")
+          }else {
+            displayPeople(FORMAT, GROUPBY, Departments[key].id, "")
+          }
         }
       } else if (GROUPBY == 'type') {
         for (const [key, value] of Object.entries(JobTypes)) {
-          displayPeople(FORMAT, GROUPBY, key)
+          // let thisTypeID = JobTypes[key].id
+          // let thisTypeName = getTaxonomyName(JobTypes, thisTypeID) 
+          // console.log("Group by Type : " + thisTypeID)
+          // console.log("Group by Name : " + thisTypeName.name)
+          if(ORDERBY === "type") {
+            firstPassCount = 0; 
+            displayPeople(FORMAT, GROUPBY, JobTypes[key].id, "firstpass")
+            displayPeople(FORMAT, GROUPBY, JobTypes[key].id, "secondpass")
+          }else {
+            displayPeople(FORMAT, GROUPBY, JobTypes[key].id, "")
+          }
         }
       } else {
-        displayPeople(FORMAT, '', '')
+        if(ORDERBY === "type") {
+          firstPassCount = 0; 
+          displayPeople(FORMAT, "", "", "firstpass")
+          displayPeople(FORMAT, "", "", "secondpass")
+        }else {
+          displayPeople(FORMAT, "", "", "")
+        }
       }
     })
 })()
