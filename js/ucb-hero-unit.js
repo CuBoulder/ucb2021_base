4b1e7ed8dbdc7632bd03fb8045d0a9db770b3921
Timur Tripp <timur@timurtripp.com>
2022-06-22 13:06:54 -0600

    #187 hero unit - adds support for YouTube videos

--- js/ucb-hero-unit.js
+++ js/ucb-hero-unit.js
@@ -1,73 +1,152 @@
-function appendVimeoPlayerScript() {
+const videoHeroSupportedSites = {
+    youtube: {
+        name: 'youtube',
+        api: 'https://www.youtube.com/iframe_api',
+        waitForLoad: false,
+        handler: function(videoURL, videoWrapperElement, videoPlayerWrapperElement, playPauseButtonElement) {
+            const youtubeAPIHandler = function() {
+                const
+                    videoPlayerElementId = videoPlayerWrapperElement.id + '_frame',
+                    videoPlayerElement = document.createElement('div');
+                videoPlayerElement.id = videoPlayerElementId;
+                videoPlayerWrapperElement.appendChild(videoPlayerElement);
+                let videoPlaying = false;
+                const videoPlayer = new YT.Player(videoPlayerElementId, {
+                    videoId: videoURL.searchParams.get('v'),
+                    playerVars: { autoplay: 1, controls: 0, mute: 1, disablekb: 1 },
+                    events: {
+                        onReady: function(event){
+                            enableVideoHeroAutoresize(videoWrapperElement, videoPlayerWrapperElement, document.getElementById(videoPlayerElementId), 800, 450);
+                            videoWrapperElement.removeAttribute('hidden');
+                        },
+                        onStateChange: function(event) {
+                            const playerState = event.data;
+                            switch(playerState) {
+                                case YT.PlayerState.PLAYING:
+                                    videoPlaying = true;
+                                    showVideoHeroPauseIcon(playPauseButtonElement);
+                                break;
+                                case YT.PlayerState.PAUSED:
+                                    videoPlaying = false;
+                                    showVideoHeroPlayIcon(playPauseButtonElement);
+                                break;
+                                case YT.PlayerState.ENDED: // This will loop the video
+                                    videoPlayer.seekTo(0);
+                                    videoPlayer.playVideo();
+                            }
+                            playPauseButtonElement.onclick = function() {
+                                if(videoPlaying)
+                                    videoPlayer.pauseVideo()
+                                else videoPlayer.playVideo();
+                            };                
+                        }
+                    }
+                }); 
+            };
+            if(window.YT)
+                youtubeAPIHandler()
+            else {
+                document.addEventListener('ucbVideoHeroYouTubeIframeAPIReady', youtubeAPIHandler);
+                window.onYouTubeIframeAPIReady = window.onYouTubeIframeAPIReady || function() { document.dispatchEvent(new CustomEvent('ucbVideoHeroYouTubeIframeAPIReady')); };
+            }
+        }
+    },
+    youtu: { // For youtu.be URLs
+        name: 'youtube',
+        api: 'https://www.youtube.com/iframe_api',
+        waitForLoad: false,
+        handler: function(videoURL, videoWrapperElement, videoPlayerWrapperElement, playPauseButtonElement) {
+            videoHeroSupportedSites.youtube.handler(new URL('https://www.youtube.com/watch?v=' + videoURL.pathname.substr(1)), videoWrapperElement, videoPlayerWrapperElement, playPauseButtonElement)
+        }
+    },
+    vimeo: {
+        name: 'vimeo',
+        api: 'https://player.vimeo.com/api/player.js',
+        waitForLoad: true,
+        handler: function(videoURL, videoWrapperElement, videoPlayerWrapperElement, playPauseButtonElement) {
+            let videoWidth = -1, videoHeight = -1, videoPlaying = false;
+            const videoPlayer = new Vimeo.Player(videoPlayerWrapperElement.id, {
+                url: videoURL + '',
+                background: true,
+                muted: true
+            });
+            // Video width and height come back from the Vimeo player API as Promises
+            // In case of error, it will default to 800x450 which is fine for 16:9 videos
+            videoPlayer.getVideoWidth().then(
+                (value) => { videoWidth = value; enableVideoHeroAutoresize(videoWrapperElement, videoPlayerWrapperElement, videoPlayer.element, videoWidth, videoHeight); },
+                (error) => { videoWidth = 800; enableVideoHeroAutoresize(videoWrapperElement, videoPlayerWrapperElement, videoPlayer.element, videoWidth, videoHeight); });
+            videoPlayer.getVideoHeight().then(
+                (value) => { videoHeight = value; enableVideoHeroAutoresize(videoWrapperElement, videoPlayerWrapperElement, videoPlayer.element, videoWidth, videoHeight); },
+                (error) => { videoHeight = 450; enableVideoHeroAutoresize(videoWrapperElement, videoPlayerWrapperElement, videoPlayer.element, videoWidth, videoHeight); });
+            // API event calls to the Vimeo player can go here
+            videoPlayer.on('loaded', function() {
+                videoWrapperElement.removeAttribute('hidden');
+            });
+            videoPlayer.on('play', function() {
+                videoPlaying = true;
+                showVideoHeroPauseIcon(playPauseButtonElement);
+            });
+            videoPlayer.on('pause', function() {
+                videoPlaying = false;
+                showVideoHeroPlayIcon(playPauseButtonElement);
+            });
+            playPauseButtonElement.onclick = function() {
+                if(videoPlaying)
+                    videoPlayer.pause()
+                else videoPlayer.play();
+            };
+        }
+    }
+};
+
+function appendVideoPlayerScript(scriptId, scriptURL) {
     const
         headElement = document.head || document.getElementsByTagName('head')[0],
         scriptElement = document.createElement('script');
     scriptElement.addEventListener('load', () => scriptElement.setAttribute('data-ucb-loaded', ''));
-    scriptElement.id = 'ucb_vimeo_player_script';
+    scriptElement.id = scriptId;
     scriptElement.type = 'text/javascript';
-    scriptElement.src = 'https://player.vimeo.com/api/player.js';
+    scriptElement.src = scriptURL;
     headElement.appendChild(scriptElement);
     return scriptElement;
 }
 
-function enableVideoHero(videoURL, videoPlayerWrapperElementId) {    
-    const scriptElement = document.getElementById('ucb_vimeo_player_script') || appendVimeoPlayerScript();
-    if(scriptElement.hasAttribute('data-ucb-loaded'))
-        createVideoHeroPlayer(videoURL, videoPlayerWrapperElementId);
-    else scriptElement.addEventListener('load', () => createVideoHeroPlayer(videoURL, videoPlayerWrapperElementId));
+function enableVideoHero(videoURLString, videoPlayerWrapperElementId) {
+    const
+        videoURL = new URL(videoURLString),
+        videoURLSplit = videoURL.hostname.split('.'),
+        videoSiteString = videoURLSplit[videoURLSplit.length - 2].toLowerCase(),
+        videoSite = videoHeroSupportedSites[videoSiteString];
+    if(!videoSite)
+        throw new Error('The video used for the video hero is from an unsupported site!');
+    const
+        scriptId = 'ucb_' + videoSite.name + '_player_script',
+        scriptElement = document.getElementById(scriptId) || appendVideoPlayerScript(scriptId, videoSite.api);
+    if(!videoSite.waitForLoad || scriptElement.hasAttribute('data-ucb-loaded'))
+        createVideoHeroPlayer(videoSite, videoURL, videoPlayerWrapperElementId);
+    else scriptElement.addEventListener('load', () => createVideoHeroPlayer(videoSite, videoURL, videoPlayerWrapperElementId));
 }
 
-function createVideoHeroPlayer(videoURL, videoPlayerWrapperElementId) {
+function createVideoHeroPlayer(videoSite, videoURL, videoPlayerWrapperElementId) {
     const
         videoPlayerWrapperElement = document.getElementById(videoPlayerWrapperElementId),
         videoWrapperElement = videoPlayerWrapperElement.parentElement,
-        playPauseButton = videoWrapperElement.querySelector('.ucb-hero-unit-video-play-pause'),
-        videoPlayer = new Vimeo.Player(videoPlayerWrapperElementId, {
-            url: videoURL,
-            background: true,
-            muted: true
-        });
-    // The video will resize automatically to fit its container
-    let videoWidth = -1, videoHeight = -1, videoPlaying = false;
-    const enableVideoHeroAutoresize = function() {
-        if(videoWidth > -1 && videoHeight > -1) {
-            resizeVideoHero(videoPlayerWrapperElement, videoPlayer, videoWidth, videoHeight);
-            window.addEventListener('resize', () => resizeVideoHero(videoPlayerWrapperElement, videoPlayer, videoWidth, videoHeight));    
-        }
-    }
-    // Video width and height come back from the Vimeo player API as Promises
-    // In case of error, it will default to 800x450 which is fine for 16:9 videos
-    videoPlayer.getVideoWidth().then(
-        (value) => { videoWidth = value; enableVideoHeroAutoresize(); },
-        (error) => { videoWidth = 800; enableVideoHeroAutoresize(); });
-    videoPlayer.getVideoHeight().then(
-        (value) => { videoHeight = value; enableVideoHeroAutoresize(); },
-        (error) => { videoHeight = 450; enableVideoHeroAutoresize(); });
-    // API event calls to the Vimeo player can go here
-    videoPlayer.on('loaded', function() {
-        videoWrapperElement.removeAttribute('hidden');
-    });
-    videoPlayer.on('play', function() {
-        videoPlaying = true;
-        playPauseButton.innerHTML = '<i class="fa fa-pause"></i>';
-    });
-    videoPlayer.on('pause', function() {
-        videoPlaying = false;
-        playPauseButton.innerHTML = '<i class="fa fa-play"></i>';
-    });
-    playPauseButton.onclick = function() {
-        if(videoPlaying)
-            videoPlayer.pause()
-        else videoPlayer.play();
-    };
+        playPauseButton = videoWrapperElement.querySelector('.ucb-hero-unit-video-play-pause');
+    videoSite.handler(videoURL, videoWrapperElement, videoPlayerWrapperElement, playPauseButton);
 }
 
-function resizeVideoHero(videoPlayerWrapperElement, videoPlayer, videoWidth, videoHeight) {
+function enableVideoHeroAutoresize(videoWrapperElement, videoPlayerWrapperElement, videoPlayerElement, videoWidth, videoHeight) {
+    if(videoWidth > -1 && videoHeight > -1) {
+        resizeVideoHero(videoWrapperElement, videoPlayerWrapperElement, videoPlayerElement, videoWidth, videoHeight);
+        window.addEventListener('resize', () => resizeVideoHero(videoWrapperElement, videoPlayerWrapperElement, videoPlayerElement, videoWidth, videoHeight));    
+    }
+}
+
+function resizeVideoHero(videoWrapperElement, videoPlayerWrapperElement, videoPlayerElement, videoWidth, videoHeight) {
     const 
-        heroElement = videoPlayerWrapperElement.parentElement.parentElement,
+        heroElement = videoWrapperElement.parentElement,
         heroWidth = heroElement.offsetWidth,
         heroHeight = heroElement.offsetHeight,
-        videoPlayerElement = videoPlayer.element,
         dimensions = calculateAspectRatioFit(videoWidth, videoHeight, heroWidth, heroHeight);
     videoPlayerElement.width = dimensions.width;
     videoPlayerWrapperElement.style.width = heroWidth + 'px';
@@ -80,4 +159,12 @@ function resizeVideoHero(videoPlayerWrapperElement, videoPlayer, videoWidth, vid
 function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
     const ratio = Math.max(maxWidth / srcWidth, maxHeight / srcHeight);
     return { width: srcWidth * ratio, height: srcHeight * ratio };
+}
+
+function showVideoHeroPauseIcon(playPauseButtonElement) {
+    playPauseButtonElement.innerHTML = '<i class="fa fa-pause"></i>';
+}
+
+function showVideoHeroPlayIcon(playPauseButtonElement) {
+    playPauseButtonElement.innerHTML = '<i class="fa fa-play"></i>';
 }
