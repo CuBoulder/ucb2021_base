1918b6956400a475be89abfcf171ed221e68d85a
Pat Brown <breepatbree@gmail.com>
2022-07-15 11:15:08 -0600

    wip: getting taxonomy ids for cats and tags for api call

--- js/ucb-related-articles.js
+++ js/ucb-related-articles.js
@@ -3,20 +3,45 @@ const relatedArticlesBlock = document.querySelector(".ucb-related-articles-block
 // Related Shown? 
 const relatedShown = relatedArticlesBlock.getAttribute('data-relatedshown') != "Off" ? true : false;
 
-// If related articles is toggled on create section, else hide it
+// If related articles is toggled on create section, run the fetch
 if(relatedShown){
-    // Tag array
-    const dataTags = relatedArticlesBlock.getAttribute('data-tags');
-    const myData = dataTags.split("  ").filter((tag)=> tag !== " " ? tag : "" );
+    // Iterate through the json data of the articles tags and categories, store the values
+    let x=0
+    let n=0
+    // Tag array - iterate through and store taxonomy ID's for fetch
+    const tagJSON = JSON.parse(relatedArticlesBlock.getAttribute('data-tagsjson'));
+    const myTagIDs = []
 
-    // Cat array
-    const dataCats = relatedArticlesBlock.getAttribute('data-cats');
-    const myCats = dataCats.split("  ").filter((cat)=> cat !== " " ? cat : "" );
+    while(tagJSON[n]!=undefined){
+        myTagIDs.push(tagJSON[n]["#cache"].tags[0])
+        n++;
+    }
+    const myTags = myTagIDs.map((id)=> id.replace(/\D/g,'')) // get only the ID #
+
+    // console.log("my tags", myTags)
+
+    // Cat array - iterate through and store taxonomy ID's for fetch
+    const catsJSON = JSON.parse(relatedArticlesBlock.getAttribute('data-catsjson'));
+    const myCatsID= [];
+    while(catsJSON[x]!=undefined){
+        myCatsID.push(catsJSON[x]["#cache"].tags[0])
+        x++;
+    }
+
+    const myCats = myCatsID.map((id)=> id.replace(/\D/g,''))// get only the ID #
+    // console.log("my cats", myCats)
+
 
     // Using tags and categories, construct an API call
+
     // Fetch
+
     // Return 3 articles that match
+
     // Render to page
+
+    // Reveal related block after 
+    relatedArticlesBlock.style.display = "block"
 
 } else {
     relatedArticlesBlock.style.display = "none";
