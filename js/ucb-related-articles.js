b00f1f6c52c408f712e26de85642458e9ad898aa
Pat Brown <breepatbree@gmail.com>
2022-07-21 14:37:53 -0600

    wip: tags seem to be working if less than 3 are gathered from categories

--- js/ucb-related-articles.js
+++ js/ucb-related-articles.js
@@ -1,4 +1,6 @@
 const relatedArticlesBlock = document.querySelector(".ucb-related-articles-block");
+let articleArrayWithScores = []
+
 
 // Related Shown? 
 const relatedShown = relatedArticlesBlock.getAttribute('data-relatedshown') != "Off" ? true : false;
@@ -15,14 +17,105 @@ function checkMatches(data, ids){
     return count
 }
 // This function takes in the tag endpoint and current array of related articles, returns the array of related articles once it has a count of 3. 
-async function getArticlesWithTags(url, array){
+async function getArticlesWithTags(url, array, articleTags){
     // console.log("=================================")
     // console.log(url)
-    // console.log("THis is my array", array)
+    console.log(array, articleTags)
     fetch(url)
     .then(response => response.json())
     .then(data=>{
-        console.log("TAG DATA", data)
+        let relatedArticlesDiv = document.querySelector('.related-articles-section')
+
+        // console.log("TAG DATA", data)
+        let returnedArticles = data.data
+        let existingIds = [];
+        // create an array of existing ids
+        array.map(article=>{
+            existingIds.push(article.id)
+        })
+
+        // remove any articles already chosen and the current article
+        let filterData = []
+        returnedArticles.map((article)=>{
+            console.log(article.id, existingIds.includes(article.id))
+            if(existingIds.includes(article.id) || article.attributes.path.alias == window.location.pathname){
+            return ''
+            } else {
+                let articleObj ={}
+                articleObj.id = article.id
+                articleObj.catMatches = checkMatches(article.relationships.field_ucb_article_tags.data, articleTags) // count the number of matches
+                articleObj.article = article 
+                filterData.push(articleObj)
+            }
+        })
+        let urlObj = {};
+        let idObj = {};
+
+        if (data.included) {
+            let filteredData = data.included.filter((url) => {
+              return url.attributes.uri !== undefined;
+            })
+            // creates the urlObj, key: data id, value: url
+            filteredData.map((pair) => {
+              urlObj[pair.id] = pair.attributes.uri.url;
+            })
+  
+            // removes all other included data besides images in our included media
+            let idFilterData = data.included.filter((item) => {
+              return item.type == "media--image";
+            })
+            // using the image-only data, creates the idObj =>  key: thumbnail id, value : data id
+            idFilterData.map((pair) => {
+              idObj[pair.id] = pair.relationships.thumbnail.data.id;
+            })
+          }
+
+        console.log("postfilter", filterData)
+        filterData.sort((a, b) => a.catMatches - b.catMatches).reverse();
+        filterData.map((article)=>{
+            console.log(article)
+            let articleCard = document.createElement('div')
+            articleCard.classList = "ucb-article-card col-sm-12 col-md-6 col-lg-4"
+            let title = article.article.attributes.title;
+            let link = article.article.attributes.path.alias;
+                    // if no thumbnail, show no image
+                    if (!article.article.relationships.field_ucb_article_thumbnail.data) {
+                        image = "";
+                      } else {
+                        //Use the idObj as a memo to add the corresponding image url
+                        let thumbId = article.article.relationships.field_ucb_article_thumbnail.data.id;
+                        image = urlObj[idObj[thumbId]];
+                      }
+            let body = ""
+            // if summary, use that
+            if( article.article.attributes.field_ucb_article_summary != null){
+                body = article.article.attributes.field_ucb_article_summary;
+            }
+    
+            // if image, use it
+            if (!article.article.relationships.field_ucb_article_thumbnail.data) {
+                imageSrc = "";
+              } else {
+                //Use the idObj as a memo to add the corresponding image url
+                let thumbId = article.article.relationships.field_ucb_article_thumbnail.data.id;
+                imageSrc = urlObj[idObj[thumbId]];
+              }
+    
+            if(link && imageSrc) {
+                image = `<a href="${link}"><img src="${imageSrc}" /></a>`;
+            }
+            let outputHTML = `
+                <div id='img' class='ucb-article-card-img'>${image}</div>
+                <div class='ucb-article-card-data'>
+                    <span class='ucb-article-card-title'><a href="${link}">${title}</a></span>
+                    <span id='body' class='ucb-related-article-card-body'>${body}</span>
+                </div>
+        `;
+    
+        articleCard.innerHTML = outputHTML
+        relatedArticlesDiv.appendChild(articleCard)
+            })
+    
     })
 }
 
@@ -121,7 +214,6 @@ if(relatedShown){
           })
         }
             let returnedArticles = data.data
-            let articleArrayWithScores = []
             // Create an array of options to render with additional checks
             returnedArticles.map((article)=> {
                 // create an object out of 
@@ -158,7 +250,8 @@ if(relatedShown){
                 articleArrayWithScores.length = 3
             } else if(articleArrayWithScores.length<3){
                 // if less than 3, grab the most tags
-                getArticlesWithTags(tagQuery,articleArrayWithScores, urlObj, idObj)
+              getArticlesWithTags(tagQuery,articleArrayWithScores, myTags);
+              
             }
 
 
@@ -168,7 +261,7 @@ if(relatedShown){
 
     // Render to page
     let relatedArticlesDiv = document.createElement('div')
-    relatedArticlesDiv.classList = "row"
+    relatedArticlesDiv.classList = "row related-articles-section"
     relatedArticlesBlock.appendChild(relatedArticlesDiv)
 
     articleArrayWithScores.map((article)=>{
