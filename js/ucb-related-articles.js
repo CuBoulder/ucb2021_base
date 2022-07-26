bbf3ec18290b19ca7415fbe3d835440eefa881ac
Pat Brown <breepatbree@gmail.com>
2022-07-26 10:13:00 -0600

    Tag filters working on both calls, template cleanup

--- js/ucb-related-articles.js
+++ js/ucb-related-articles.js
@@ -3,9 +3,17 @@ const excludeTagEl = document.querySelector("#ucb-related-articles-exclude-tags"
 const excludeCatEl = document.querySelector("#ucb-related-articles-exclude-categories")
 
 // Get the tax ids for excluded Cats and Tags
-const excludeTagArr = excludeTagEl.getAttribute('data-excludetags').split(',').filter(Number)
-const excludeCatArr = excludeCatEl.getAttribute('data-excludecategories').split(',').filter(Number)
+let excludeCatArr = [];
+if (excludeCatEl){
+    excludeCatArr = excludeCatEl.getAttribute('data-excludecategories').split(',').filter(Number)
+}
 
+let excludeTagArr = [];
+if (excludeTagEl){
+    excludeTagArr = excludeTagEl.getAttribute('data-excludetags').split(',').filter(Number)
+}
+
+// Global variable to store articles that are good matches. Danger!
 let articleArrayWithScores = []
 
 // Related Shown? 
@@ -42,16 +50,56 @@ async function getArticlesWithTags(url, array, articleTags ,numLeft){
         let filterData = []
         returnedArticles.map((article)=>{
 
+            let thisArticleCats = article.relationships.field_ucb_article_categories.data
+                let thisArticleTags = article.relationships.field_ucb_article_tags.data
+                let urlCheck = article.attributes.path.alias;
+                let toInclude = true;
+                //remove excluded category & tagss
+                if(thisArticleTags.length){ // if there are categories
+                    thisArticleTags.forEach((tag)=>{
+                        let id = tag.meta.drupal_internal__target_id.toString();
+                        console.log(id)
+                         if(excludeCatArr.includes(id)){
+                            toInclude = false;
+                            return
+                         }
+                    }) 
+                }
+
+                if(thisArticleCats.length){ // if there are categories
+                    thisArticleCats.forEach((category)=>{ // check each category
+                        let id = category.meta.drupal_internal__target_id.toString();
+                        console.log('cat id', id)                        
+                         if(excludeCatArr.includes(id)){ // if excluded, do not proceed
+                            // console.log('i have an included id')
+                            toInclude = false;
+                            return
+                         } 
+                         if( urlCheck == window.location.pathname) { // if same article, do not proceed
+                            //  console.log("im the current article! ignore me!")
+                             toInclude = false
+                            return;
+                         }  // proceed
+                             // create an object out of 
+                             // add to running array of possible matches
+                         
+                    }) 
+                }
+
             // console.log(article.id, existingIds.includes(article.id))
             if(existingIds.includes(article.id) || article.attributes.path.alias == window.location.pathname ){
-                return ''
+                toInclude = false
             // filter on categories
-            }else{
+            } 
+            if(toInclude){
                 let articleObj ={}
                 articleObj.id = article.id
                 articleObj.catMatches = checkMatches(article.relationships.field_ucb_article_tags.data, articleTags) // count the number of matches
                 articleObj.article = article 
                 filterData.push(articleObj)
+            }
+            else{
+                return
             }
         })
 
@@ -231,6 +279,7 @@ if(relatedShown){
                         console.log(id)
                          if(excludeCatArr.includes(id)){
                             toInclude = false;
+                            return
                          }
                     }) 
                 }
@@ -247,7 +296,7 @@ if(relatedShown){
                          if( urlCheck == window.location.pathname) { // if same article, do not proceed
                             //  console.log("im the current article! ignore me!")
                              toInclude = false
-                            return '';
+                            return;
                          }  // proceed
                              // create an object out of 
                              // add to running array of possible matches
